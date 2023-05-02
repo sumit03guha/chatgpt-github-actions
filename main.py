a29fcbba5f6b782551d1765ac7e2aac042f52b40
Sumit Guha <sumit03guha@gmail.com>
2023-05-02 23:21:48 +0530

    changed from completion to chat-completion model

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def files():
                 filename, ref=commit.sha).decoded_content
 
             # Sending the code to ChatGPT
-            response = openai.Completion.create(
+            response = openai.ChatCompletion.create(
                 engine=args.openai_engine,
                 prompt=(
                     f"""Please review the following code changes in this GitHub pull request delimited by the triple backticks
